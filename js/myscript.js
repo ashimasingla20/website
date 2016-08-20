4e89664129178bbae6a6e7c7c06c7e3d218dab51
Ashima <ashima.singla@zomato.com>
2016-08-20 20:25:34 +0530

    fix

--- js/myscript.js
+++ js/myscript.js
@@ -81,3 +81,18 @@ $(function() {
     });
 
 });
+//modal-popup
+/*$('#item-wrap a').magnificPopup({
+
+       type:'inline',
+       fixedContentPos: false,
+       removalDelay: 200,
+       showCloseBtn: false,
+       mainClass: 'mfp-fade'
+
+    });*/
+
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+            e.preventDefault();
+            $.magnificPopup.close();
+    });
